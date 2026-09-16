1e67bface7db809934a8fc08eb86ff8295ba77c9
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-09-16 22:40:34 +0400

    first commit

---
+++ src/main/java/az/shopery/filenet_ms/FilenetMsApplication.java
@@ -0,0 +1,12 @@
+package az.shopery.filenet_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilenetMsApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(FilenetMsApplication.class, args);
+	}
+}
---
+++ src/test/java/az/shopery/filenet_ms/FilenetMsApplicationTests.java
@@ -0,0 +1,13 @@
+package az.shopery.filenet_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilenetMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
